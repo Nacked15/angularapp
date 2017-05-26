7f855b38e2fdfed8dd26aa4e998a5c5afbf64c19
Luis Yama <jluis.yama@gmail.com>
2017-05-26 09:07:04 -0600

    Upload updates

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,20 +2,26 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, JsonpModule } from '@angular/http';
+import { routing, appRoutingProviders } from './app.routing';
 
 import { AppComponent }  from './app.component';
 import { FavoritosListComponent }  from './components/favoritos-list.component';
+import { FavoritoDetailComponent } from './components/favorito-detail.component';
 
 @NgModule({
-  imports:      [ 
+  imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
-     ],
+    HttpModule,
+    routing
+  ],
   declarations: [ 
     AppComponent,
-    FavoritosListComponent ],
-  bootstrap:    [ AppComponent ]
+    FavoritosListComponent,
+    FavoritoDetailComponent
+  ],
+  providers: [ appRoutingProviders ],
+  bootstrap: [ AppComponent ]
 })
  
 export class AppModule { }
